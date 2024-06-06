98aaaadba74907ea7d5ef49117f4cdee34f2385a
zainablfz <144009548+zainablfz@users.noreply.github.com>
2024-06-06 15:28:42 +0200

    post-route brew ingredients #8

--- server.js
+++ server.js
@@ -10,14 +10,12 @@ app.set('views', './views')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 
-
 // Variabelen definiëren
-const apiUrl = `"https://potion-api-jet.vercel.app/"`;
-const potionsUrl = `${apiUrl}potions`;
-const ingredientsUrl = `${apiUrl}ingredients`;
+const apiUrl = "https://potion-api-jet.vercel.app/",
+      potionsUrl = `${apiUrl}potions`,
+      ingredientsUrl = `${apiUrl}ingredients`;
 
-
-// Het maken van routes
+// Routes aanmaken
 app.get('/', (request, response) => {
   Promise.all([
     fetchJson(`${potionsUrl}`),
@@ -25,6 +23,68 @@ app.get('/', (request, response) => {
   ]).then(([potions, ingredients]) => {
 
   response.render('index', {potions, ingredients });
+  })
+})
+
+app.get('/brew', (request, response) => {
+  Promise.all([
+    fetchJson(`${potionsUrl}`),
+    fetchJson(`${ingredientsUrl}`)
+  ]).then(([potions, ingredients]) => {
+
+  response.render('brew', {potions, ingredients });
+  })
+})
+
+app.get('/potions', (request, response) => {
+  Promise.all([
+    fetchJson(`${potionsUrl}`),
+    fetchJson(`${ingredientsUrl}`)
+  ]).then(([potions, ingredients]) => {
+
+  response.render('potions', {potions, ingredients });
+  })
+})
+
+app.get('/potion/:id', (request, response) =>{
+  fetchJson(`${potionsUrl}/${request.params.id}`).then((potion) =>{
+    response.render('potion', {potion});
+  })
+})
+
+app.get('/nasty-potion', (request, response) =>{
+  response.render('nasty-potion')
+})
+
+app.post('/brew', (request, response) => {
+  fetchJson(`${potionsUrl}`).then((potions) => {
+    let ingredients = request.body.ingredients;
+    let matchFound = false;
+    let matchedPotionId = null;
+
+    if (!Array.isArray(ingredients)) {
+      return response.status(400).send('You need 2 or more ingredients to brew a potion.');
+    }
+
+    ingredients = ingredients.map(Number);
+
+    function sortArray(arr) {
+      return arr.map(Number).sort((a, b) => a - b); 
+    }
+
+    for (let i = 0; i < potions.length; i++){
+      const sortPotionIngredients = sortArray(potions[i].ingredients);
+      if (JSON.stringify(ingredients) === JSON.stringify(sortPotionIngredients)){
+        matchFound = true;
+        matchedPotionId = potions[i].id;
+      } 
+    }
+    
+    if (matchFound) {
+      response.redirect(301, `/potion/${matchedPotionId}`)
+    } else {
+      response.redirect(301, '/nasty-potion')
+    }
   })
 })
 
